a0766764507d46727fca8444a509e96146075179
saipavan-1919 <114296009+saipavan-1919@users.noreply.github.com>
2022-10-14 16:51:26 +0530

    Update 1_reading_image.py

--- AI/ComputerVision/1_reading_image.py
+++ AI/ComputerVision/1_reading_image.py
@@ -1,1 +1,10 @@
+ # importing opencv library
+import cv2
 
+# reading image
+img = cv2.imread("sample1.jpg");
+# showing image
+cv2.imshow("image",img);
+# making image to hold for infinite time 
+# (if we give some value instead of 0 it will wait for that many milli seconds)
+cv2.waitKey(0);
